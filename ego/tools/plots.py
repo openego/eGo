42e213d32d9f9c0e81bbac89043d256b5257223b
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-09-08 11:48:02 +0200

    add first changes

--- ego/tools/plots.py
+++ ego/tools/plots.py
@@ -8,4 +8,21 @@ ToDo:
 
 
 """
+__copyright__ = "tba"
+__license__ = "tba"
+__author__ = "tba"
 
+from etrago.appl import (plot_line_loading, plot_stacked_gen,
+                                     add_coordinates, curtailment, gen_dist,
+                                     storage_distribution)
+def make_all_plots(eTraGo_network):
+	# make a line loading plot
+	plot_line_loading(eTraGo_network)
+
+	# plot stacked sum of nominal power for each generator type and timestep
+	plot_stacked_gen(eTraGo_network, resolution="MW")
+
+	# plot to show extendable storages
+	storage_distribution(eTraGo_network)
+
+	return
