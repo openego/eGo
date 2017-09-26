9ccadc621642312cf1092c085f6a017c30648b49
WolfBunke <wolf-dieter.bunke@uni-flensburg.de>
2017-09-26 11:24:07 +0200

    delete path string

--- ego/tools/utilities.py
+++ ego/tools/utilities.py
@@ -5,7 +5,6 @@ import os
 
 def get_scenario_setting():
     path = os.getcwd()
-    #path = '/home/dozeumbuw/eTraGo/src/eGo/ego/'
     with open(path +'scenario_setting.json') as f:
       scenario_setting = json.load(f)
 
