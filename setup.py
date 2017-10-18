131b89f7de9b1aa22e4fc74eaffe53a8d1d1bd91
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-10-18 16:58:59 +0200

    add egoio version 2.11

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='eGo',
 	  url='https://github.com/openego/eGo',
       license="GNU Affero General Public License Version 3 (AGPL-3.0)",
       packages=find_packages(),
-      install_requires=['egoio == 0.2.0',
+      install_requires=['egoio == v0.2.11',
                         'egopowerflow == 0.0.4'],
 	dependency_links=['git+ssh://git@github.com/openego/eTraGo.git@dev#egg=eTraGo',
 			 'git+ssh://git@github.com/openego/PyPSA.git@dev#egg=PyPSA']
