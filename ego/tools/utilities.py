212ca15bf94383ee8b6a004254c20e893a83dc83
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-11-06 18:25:58 +0100

    update json scenario reader

--- ego/tools/utilities.py
+++ ego/tools/utilities.py
@@ -3,9 +3,33 @@ import os
 
 # import scenario settings **args
 
-def get_scenario_setting():
+def get_scenario_setting(json_file='scenario_setting.json'):
+    """ Get and open json file with scenaio settings of eGo
+
+    Parameter:
+    ----------
+
+    json_file (str):
+        default: 'scenario_setting.json'
+        Name of scenario setting json file
+    """
     path = os.getcwd()
-    with open(path +'scenario_setting.json') as f:
-      scenario_setting = json.load(f)
+    with open(path +'/'+json_file) as f:
+      scn_set = json.load(f)
+
+    if scn_set['global'].get('eTraGo') == True:
+        print('Use eTraGo settings')
+
+    if scn_set['global'].get('eDisGo') == True:
+        print('Use eDisGo settings')
+
+    return scn_set
+
+
+def get_etrago_scnario():
+
+
+
+
 
     return scenario_setting
