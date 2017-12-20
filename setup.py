67234ea73ad1c8f72f7d7916c99a4a59b8e3388b
WolfBunke <wolf-dieter.bunke@uni-flensburg.de>
2017-12-20 17:13:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,11 +23,12 @@ setup(name='eGo',
                         'geopandas==0.3.0',
                         'Rtree==0.8.3',
                         'plotly==2.2.3',
+			
                         'folium==0.5.0',
                         'matplotlib >= 1.5.3, <=1.5.3'],
-	dependency_links=['ggit+https://git.github.com/openego/PyPSA.git@dev#egg=PyPSA',
-			 'git+https://git.github.com:python-visualization/folium.git@master#egg=folium',
-			 'git+https://git.github.com:openego/eTraGo.git@dev#egg=eTraGo'],
+	dependency_links=['git+https://git.github.com/openego/PyPSA.git@dev#egg=PyPSA',
+			 'git+https://git.github.com:python-visualization/folium.git@master#egg=folium-0.5.0+98.g106d829',
+			 'git+https://git.github.com:openego/eTraGo.git@dev#egg=eTraGo-0.5'],
 	 extras_require={
         'docs': [
             'sphinx >= 1.4',
