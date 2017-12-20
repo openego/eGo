6b4040f21b3e75df7cb1017f225e6f27cd15be34
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-12-20 15:41:04 +0100

    add web plotting from console

--- ego/ego_main.py
+++ ego/ego_main.py
@@ -2,19 +2,9 @@
 This is the application file for the tool eGo. The application eGo calculates the distribution and transmission grids
 of eTraGo and eDisGo.
 
+
 Warrning: This Repository is underconstruction and work in progress (wip)
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation; either version 3 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 __copyright__ = "Flensburg University of Applied Sciences, Europa-Universität Flensburg, Centre for Sustainable Energy Systems"
 __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
@@ -23,13 +13,20 @@ __author__ = "wolfbunke"
 from etrago.appl import etrago
 from tools.plots import (make_all_plots,plot_line_loading, plot_stacked_gen,
                                      add_coordinates, curtailment, gen_dist,
-                                     storage_distribution)
-from tools.utilities import get_scenario_setting, get_time_steps
+                                     storage_distribution, igeoplot)
+from tools.utilities import get_scenario_setting, get_time_steps, bus_by_country
 #from eDisGo import ...
+# use spects
 # import country selection
-from tools.utilities import bus_by_country
 import pandas as pd
 from tools.io import geolocation_buses
+from egoio.tools import db
+from sqlalchemy.orm import sessionmaker
+import logging
+logger = logging.getLogger(__name__)
+
+
+
 
 
 """
@@ -49,22 +46,14 @@ test = geolocation_buses(network = eTraGo, section='oedb')
 
 #plot_line_loading(eTraGo)
 
-
-make_all_plots(eTraGo)
-
-
 # write results to excel
 #hv_generator_results(eTraGo)
-"""
 
-
-"""
 
 
 if __name__ == '__main__':
     # import scenario settings **args of eTraGo
     args = get_scenario_setting(json_file='scenario_setting.json')
-
 
 
     # start eTraGo calculation
@@ -73,12 +62,17 @@ if __name__ == '__main__':
     # add country code to bus and geometry (shapely)
     # eTraGo.buses = eTraGo.buses.drop(['country_code','geometry'], axis=1)
     ##eTraGo.lines.info()
+    conn = db.connection(section=args['eTraGo']['db'])
+    Session = sessionmaker(bind=conn)
+    session = Session()
 
 
-    network =eTraGo
-    test = geolocation_buses(network = eTraGo, section='oedb')
+    igeoplot(eTraGo, session)
 
+    #network =eTraGo
+    #test = geolocation_buses(network = eTraGo, section='oedb')
 
+    #igeoplot(network =eTraGo)
 
     # plots
     # make a line loading plot
@@ -90,9 +84,6 @@ if __name__ == '__main__':
     # plot to show extendable storages
     storage_distribution(eTraGo)
 
-
-
-
     # start calculations (eTraGo and eDisGo)
     # rename resulting network container to eTraGo
     #eTraGo = etrago(args2)
@@ -100,7 +91,7 @@ if __name__ == '__main__':
 
 
     # test call plot function
-    make_all_plots(eTraGo)
+    #make_all_plots(eTraGo)
 
 
 
