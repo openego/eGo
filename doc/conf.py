e55a57a1c270fa655fa9d55dff7f4eb2543f1ca0
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-03-29 14:25:31 +0200

    add test func

--- doc/conf.py
+++ doc/conf.py
@@ -89,6 +89,23 @@ extlinks = {'pandas':('http://pandas.pydata.org/pandas-docs/stable/api.html#%s',
 }
 
 # test oedb implementation
+
+def rstjinja(app, docname, source):
+    """
+    Render our pages as a jinja template for fancy templating goodness.
+    """
+    # Make sure we're outputting HTML
+    if app.builder.format != 'html':
+        return
+    src = source[0]
+    rendered = app.builder.templates.render_string(
+        src, app.config.html_context
+    )
+    source[0] = rendered
+
+def setup(app):
+    app.connect("source-read", rstjinja)
+    
 import requests
 oep_url= 'http://oep.iks.cs.ovgu.de/'
 
