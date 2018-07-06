70aea89d4185f6c865807bd1e97392adb347cc0a
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-07-06 12:12:21 +0200

    change to pep8 header style

--- ego/tools/plots.py
+++ ego/tools/plots.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2018 Europa-Universität Flensburg,
+# Flensburg University of Applied Sciences,
+# Centre for Sustainable Energy Systems
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# File description
 """Module which collects useful functions for plotting eTraGo, eDisGo and
 eGo results.
 
@@ -5,10 +24,6 @@ ToDo
 ----
 - Implement plotly for iplot
 """
-__copyright__ = "tba"
-__license__ = "tba"
-__author__ = "tba"
-
 import numpy as np
 import pandas as pd
 import os
@@ -32,6 +47,11 @@ if not 'READTHEDOCS' in os.environ:
 
 import logging
 logger = logging.getLogger('ego')
+
+__copyright__ = "Flensburg University of Applied Sciences, Europa-Universität"\
+    "Flensburg, Centre for Sustainable Energy Systems"
+__license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__author__ = "wolfbunke"
 
 
 # plot colore of Carriers
