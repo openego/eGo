44e9beecce7ad82bc4d6dbbc245c8386a9a771c8
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-07-06 12:08:03 +0200

    change to pep8 header style

--- ego/tools/results.py
+++ ego/tools/results.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
+# Copyright 2016-2018 Europa-Universität Flensburg,
+# Flensburg University of Applied Sciences,
+# Centre for Sustainable Energy Systems
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# File description
 """This module include the results functions for analyze and creating results
 based on eTraGo or eDisGo for eGo.
-
 
 ToDo
 ----
@@ -9,10 +26,6 @@ ToDo
 
 
 """
-__copyright__ = "Flensburg University of Applied Sciences, Europa-Universität"\
-    "Flensburg, Centre for Sustainable Energy Systems"
-__license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
-__author__ = "wolfbunke"
 
 import io
 import os
@@ -23,6 +36,11 @@ if not 'READTHEDOCS' in os.environ:
     import pandas as pd
     import numpy as np
     from tools.economics import get_generator_investment
+
+__copyright__ = "Flensburg University of Applied Sciences, Europa-Universität"\
+    "Flensburg, Centre for Sustainable Energy Systems"
+__license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__author__ = "wolfbunke"
 
 
 def create_etrago_results(network, scn_name):
