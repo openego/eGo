a68210efd7fda4cc9a183e8b8707bc72ac5e78dd
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-07-17 17:51:49 +0200

    update storage_costs

--- ego/tools/storages.py
+++ ego/tools/storages.py
@@ -116,3 +116,33 @@ def etrago_storages(network):
     storages = total_storage_charges(network=network)
 
     return storages
+
+
+def etrago_storages_investment(network):
+    """Calculate storage investment costs of eTraGo
+
+    Parameters
+    ----------
+    network_etrago: :class:`etrago.tools.io.NetworkScenario`
+        eTraGo network object compiled by :meth:`etrago.appl.etrago`
+
+
+    Returns
+    -------
+    storage_costs : numeric
+        Storage costs of selected snapshots in [EUR]
+
+    """
+    # provide storage installation costs
+    if sum(network.storage_units.p_nom_opt) != 0:
+        installed_storages = \
+            network.storage_units[network.storage_units.p_nom_opt != 0]
+        storage_costs = sum(
+            installed_storages.capital_cost *
+            installed_storages.p_nom_opt)
+        print(
+            "Investment costs for all storages in selected snapshots [EUR]:",
+            round(
+                storage_costs,
+                2))
+    return storage_costs
