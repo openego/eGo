f39f3c4a84c935f7d609075978d8d6e951e06840
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-07-18 12:06:08 +0200

    add and start with json docu

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 
 setup(name='eGo',
-      version='0.2',
+      version='0.2.0',
       author='wolfbunke, maltesc',
       author_email='',
       description=("A python package for distribution and transmission"
