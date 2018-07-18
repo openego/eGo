29e0f1be18fc040b48bec233851264023c934c95
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-07-18 09:17:29 +0200

    update file

--- ego/tools/io.py
+++ ego/tools/io.py
@@ -326,7 +326,7 @@ class eGo(eDisGoResults):
         # super().__init__(eDisGo)
         self.total = pd.DataFrame()
         # add total results here
-        self.total_investment_costs = investment_costs(self.etrago_network)
+        self.total_investment_costs = pd.DataFrame()  # TODO
         self.total_operation_costs = pd.DataFrame()  # TODO
 
         pass
