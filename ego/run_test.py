fa7b7d765b86acb4e404fc84b0120cfd9d6a225f
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-07-19 14:32:20 +0200

    add file for testing

---
+++ ego/run_test.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from tools.io import eGo
+import sys
+from pycallgraph import PyCallGraph
+from pycallgraph.output import GraphvizOutput
+
+
+def main():
+    graphviz = GraphvizOutput()
+    graphviz.output_file = 'basic.png'
+    date = str(datetime.now())
+    print(date)
+    with PyCallGraph(output=graphviz):
+
+        ego = eGo(jsonpath='scenario_setting_solver_option.json')
+
+        print(ego.etrago.storage_charges)
+
+        print(ego.etrago.storage_investment_costs)
+        ego.etrago.storage_investment_costs.to_csv(
+            date+'etrago_storage_costs.csv')
+        print(ego.etrago.grid_investment_costs)
+        ego.etrago.grid_investment_costs.to_csv(date+'etrago_grid_costs.csv')
+        # test eTraGo plot and functions
+
+        print(ego.edisgo.grid_investment_costs)
+        ego.edisgo.grid_investment_costs.to_csv(date+'edisgo_gridscosts.csv')
+
+        ego.etrago_line_loading()
+        ego.etrago_stacked_gen()
+        ego.etrago_gen_dist()
+        ego.etrago_storage_distribution()
+        ego.etrago_voltage()
+
+        # object size
+        print(sys.getsizeof(ego))
+
+    print(str(datetime.now()))
+
+
+if __name__ == '__main__':
+    main()
