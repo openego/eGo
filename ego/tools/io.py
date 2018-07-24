1621da7e1da707af0100e3811a01b4576041592d
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-07-24 14:04:58 +0200

    out comments storage_investment_costs

--- ego/tools/io.py
+++ ego/tools/io.py
@@ -179,8 +179,8 @@ class eTraGoResults(egoBasic):
         # add selected results to Results container
 
         self.etrago = pd.DataFrame()
-        self.etrago.storage_investment_costs = etrago_storages_investment(
-            self.etrago_network, self.json_file)
+        # self.etrago.storage_investment_costs = etrago_storages_investment(
+        #    self.etrago_network, self.json_file)
         self.etrago.storage_charges = etrago_storages(self.etrago_network)
         self.etrago.operating_costs = etrago_operating_costs(
             self.etrago_network)
