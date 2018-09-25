fcf1c6bbcc9bb9672038e4f53fc108844a7e9cc4
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-09-25 15:41:21 +0200

    update legend of cluster grids

--- ego/tools/plots.py
+++ ego/tools/plots.py
@@ -922,9 +922,11 @@ def igeoplot(ego, tiles=None, geoloc=None, args=None):
     mp.add_child(colormap)
     mp.add_child(colormap2)
 
-    # Add layer groups
+    # add legend
+    # add layer groups
     bus_group.add_to(mp)
     line_group.add_to(mp)
+
     if ego.json_file['eGo']['eDisGo'] is True:
 
         repgrid_group.add_to(mp)
@@ -939,6 +941,10 @@ def igeoplot(ego, tiles=None, geoloc=None, args=None):
         title='Fullscreen',
         title_cancel='Exit me',
         force_separate_button=True).add_to(mp)
+
+    if ego.json_file['eGo']['eDisGo'] is True:
+        mp = iplot_griddistrict_legend(
+            mp=mp, repre_grids=repre_grids, start=True)
 
     # Save Map
     html_dir = 'results/html'
@@ -1023,3 +1029,128 @@ def _storage_distribution(network, ax, fig, scaling=1, filename=None):
             ax=ax,
             line_widths=0.3
         )
+
+
+def iplot_griddistrict_legend(mp, repre_grids, start=False):
+    """ Add legend to iplot function.
+
+    """
+    #from branca.element import Template, MacroElement
+    from string import Template
+
+    if start:
+
+        legends = []
+        for name, row in repre_grids.groupby(['cluster_id', 'color']).count().iterrows():
+
+            color = name[1]
+            grid_no = name[0]
+
+            entry = """<li><span style = 'background:{};opacity:0.7;' >
+                     </span > Represented by Grid {} </li>""".format(color, grid_no)
+
+            legends.append(entry)
+
+        legend = "\n"
+        legend = legend.join(legends)
+
+        temp_1 = """
+
+        <!doctype html>
+        <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <title>open_eGo interactiv result plot</title>
+          <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+          <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+          <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+          <script>
+          $( function() {
+            $( "#maplegend" ).draggable({
+                            start: function (event, ui) {
+                                $(this).css({
+                                    right: "auto",
+                                    top: "auto",
+                                    bottom: "auto"
+                                });
+                            }
+                        });
+        });
+
+          </script>
+        </head>
+        <body>
+        """
+
+        temp_2 = """
+
+        <div id='maplegend' class='maplegend'
+            style='position: absolute; z-index:9999; border:2px solid grey; background-color:rgba(255, 255, 255, 0.8);
+             border-radius:6px; padding: 10px; font-size:14px; right: 20px; bottom: 20px;'>
+
+        <div class='legend-title'>Legend (draggable)</div>
+        <div class='legend-scale'>
+          <ul class='legend-labels'>
+
+            $legend
+
+          </ul>
+        </div>
+        </div>
+        </body>
+        </html>
+        """
+
+        temp_3 = """
+        <style type='text/css'>
+          .maplegend .legend-title {
+            text-align: left;
+            margin-bottom: 5px;
+            font-weight: bold;
+            font-size: 90%;
+            }
+          .maplegend .legend-scale ul {
+            margin: 0;
+            margin-bottom: 5px;
+            padding: 0;
+            float: left;
+            list-style: none;
+            }
+          .maplegend .legend-scale ul li {
+            font-size: 80%;
+            list-style: none;
+            margin-left: 0;
+            line-height: 18px;
+            margin-bottom: 2px;
+            }
+          .maplegend ul.legend-labels li span {
+            display: block;
+            float: left;
+            height: 16px;
+            width: 30px;
+            margin-right: 5px;
+            margin-left: 0;
+            border: 1px solid #999;
+            }
+          .maplegend .legend-source {
+            font-size: 80%;
+            color: #777;
+            clear: both;
+            }
+          .maplegend a {
+            color: #777;
+            }
+        </style>
+        """
+        t = Template(temp_2)
+        temp_2 = t.substitute(legend=legend)
+
+        temps = temp_1+temp_2+temp_3
+
+        #macro = MacroElement(**leg)
+        #macro._template = Template(template)
+        # return mp.get_root().add_child(macro)
+        return mp.get_root().html.add_child(folium.Element(temps))
