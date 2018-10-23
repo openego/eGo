4cf2b2dde2ac3b33511358c1ee3be8539cf30d05
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-10-23 10:32:27 +0200

    add new version

--- setup.py
+++ setup.py
@@ -9,15 +9,10 @@ __copyright__ = ("Flensburg University of Applied Sciences, "
 __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __author__ = "wolf_bunke, maltesc"
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-requirements = parse_requirements("ego_dependencies.txt", session="")
 
 setup(
     name='eGo',
-    version='0.3.0.24',
+    version='0.3.1dev',
     author='wolfbunke, maltesc',
     author_email='wolf-dieter.bunke@uni-flensburg.de',
     description=("A cross-grid-level electricity grid and storage "
@@ -28,13 +23,40 @@ setup(
     packages=find_packages(),
     package_dir={'ego': 'ego'},
     include_package_data=True,
-    install_requires=[str(b.req) for b in requirements],
-    dependency_links=[str(c._link) for c in requirements if c._link],
+    install_requires=['egoio == 0.4.5',
+                      'ding0 == v0.1.9',
+                      'pycallgraph', 
+                      'scikit-learn == 0.19.0',
+                      'pandas ==0.20.3',
+                      'eDisGo == v0.0.6',
+                      'eTraGo == 0.7.0',
+                      'pypsa==0.11.0fork',
+                      'sqlalchemy >= 1.0.15, <= 1.1.4',
+                      'geoalchemy2 >= 0.3.0, <=0.4.0',
+                      'matplotlib >= 1.5.3, <=1.5.3',
+                      'tsam==0.9.9',
+                      'geopandas',
+                      'matplotlib == 3.0.0',
+                      'Rtree',
+                      'descartes',
+                      'pyproj',
+                      'plotly==2.2.3',
+                      'shapely',
+                      'multiprocess',
+                      'folium',
+                      'oedialect'
+                      ],
+    dependency_links=[
+        ('git+https://git@github.com/openego/PyPSA.git'
+         '@master#egg=pypsa-0.11.0fork')],
     extras_require={
         'doc': [
             'sphinx >= 1.4',
             'sphinx_rtd_theme',
-            'sphinxcontrib-httpdomain']},
+            'sphinxcontrib-httpdomain',
+            'numpydoc == 0.7.0',
+            'aiohttp_jinja2',
+            'sphinx-jsondomain']},
     package_data={
         'ego': [os.path.join('tools', '*.csv')],
         'ego': [os.path.join('tools', '*.json')],
@@ -42,5 +64,22 @@ setup(
         'ego.data': ['*.csv']
     }
     )
-    
+   
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
   
