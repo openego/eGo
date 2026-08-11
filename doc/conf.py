519a9bac53cfd5310c58688de29fb3957d73484e
HelenP2004 <helen_prenzel@t-online.de>
2026-08-11 12:15:54 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -54,7 +54,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
+    "sphinxcontrib.jquery",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
