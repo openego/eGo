8ac7083d2b25102cf19a49d3d7b4f64951ff686d
HelenP2004 <helen_prenzel@t-online.de>
2026-08-25 10:04:22 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -157,7 +157,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "eGo"
-copyright = "2015-2018, open_eGo-Team"
+copyright = "2025-2026, open_eGo-Team"
 author = "open_eGo-Team"
 
 
